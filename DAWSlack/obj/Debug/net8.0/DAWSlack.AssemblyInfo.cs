a0b4e82a172fc38a8baafc652d3fc60f58d39672
teodoranae <teodoranae01@gmail.com>
2024-12-16 18:16:23 +0200

    UpdatedUsername

--- DAWSlack/obj/Debug/net8.0/DAWSlack.AssemblyInfo.cs
+++ DAWSlack/obj/Debug/net8.0/DAWSlack.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAWSlack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01ba16d7aa8ef03ef1865f92a4d92f7761b241ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+489b27b63ff7634f1af1a1537465c0bd1bc94e2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAWSlack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAWSlack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
